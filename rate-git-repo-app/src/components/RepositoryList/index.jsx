8a6fcdbf968acf6db39e30730c122b3cd84d047d
Rizvan <rizvan.chalilovas@proton.me>
2023-10-13 16:53:46 +0300

    rate-git-repo-app - move RepositoryList component into separate fodler

--- rate-git-repo-app/src/components/RepositoryList/index.jsx
+++ rate-git-repo-app/src/components/RepositoryList/index.jsx
@@ -1,7 +1,7 @@
 import { FlatList, View, StyleSheet, ActivityIndicator } from 'react-native';
 import RepositoryItem from './RepositoryItem';
-import useRepositories from '../hooks/useRepositories';
-import Text from './Text';
+import useRepositories from '../../hooks/useRepositories';
+import Text from '../Text';
 
 const styles = StyleSheet.create({
   separator: {
