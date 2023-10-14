03860ab651edb30bdfdb9bfadd52544c598893ff
Rizvan <rizvan.chalilovas@proton.me>
2023-10-14 18:37:36 +0300

    rate-git-repo-app - implement sign-in form mutation

--- rate-git-repo-app/src/components/SignIn.jsx
+++ rate-git-repo-app/src/components/SignIn.jsx
@@ -4,6 +4,7 @@ import * as yup from 'yup';
 import FormikTextInput from './FormikTextInput';
 import Text from './Text';
 import theme from '../theme';
+import useSignIn from '../hooks/useSignIn';
 
 const initialValues = {
   username: '',
@@ -68,8 +69,18 @@ const SignInForm = ({ onSubmit }) => {
 };
 
 const SignIn = () => {
-  const onSubmit = (values) => {
-    console.log('submited', values);
+  const [signIn] = useSignIn();
+
+  const onSubmit = async (values) => {
+    const { username, password } = values;
+    try {
+      const { data } = await signIn({ username, password });
+      console.log('SignIn success block');
+      console.log(data);
+    } catch (error) {
+      console.log('SignIn error block');
+      console.log(error);
+    }
   };
 
   return (
---
+++ rate-git-repo-app/src/hooks/useSignIn.js
@@ -0,0 +1,14 @@
+import { useMutation } from '@apollo/client';
+import { AUTHENTICATE } from '../graphql/mutations';
+
+const useSignIn = () => {
+  const [authenticate, result] = useMutation(AUTHENTICATE);
+
+  const signIn = async ({ username, password }) => {
+    return authenticate({ variables: { credentials: { username, password } } });
+  };
+
+  return [signIn, result];
+};
+
+export default useSignIn;
